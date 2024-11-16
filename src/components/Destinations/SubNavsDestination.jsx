e4ad8e2020886a1a65422f24cd59bd4cb5a23942
DesaiPriyansh19 <priyanshdesai62@gmail.com>
2024-11-16 13:30:50 +0530

    minor-responsive changes

--- src/components/Destinations/SubNavsDestination.jsx
+++ src/components/Destinations/SubNavsDestination.jsx
@@ -6,15 +6,13 @@ export default function SubNavsDestination() {
   return (
     <nav className="bg-white sticky top-0 z-[19] shadow-inner">
       <div className="max-w-7xl mx-auto px-4 py-4">
-        <p className="pb-2 italic underline font-medium em:hidden block">
-          Sub Menus
-        </p>
-        <ul className="flex flex-col em:flex-row items-start  em:items-center justify-center text-[.7rem] em:text-sm uppercase leading-6 font-normal  em:space-x-8">
+     
+        <ul className="flex flex-row em:flex-row items-start  em:items-center justify-center text-[.7rem] em:text-sm uppercase leading-6 font-normal  em:space-x-8">
           {sections.map((item, idx) => (
             <button
               key={idx}
               className={`text-gray-500 uppercase em:py-0 py-2  em:border-b-0 ${
-                idx === sections.length - 1 ? "border-b-0" : "border-b"
+                idx === sections.length - 1 ? "border-b-0" : "border-b-0"
               }  em:w-auto  w-full hover:text-med-green`}
             >
               {item}
--- src/components/ViewAllDestinations/SubNavofViewall.jsx
+++ src/components/ViewAllDestinations/SubNavofViewall.jsx
@@ -2,17 +2,15 @@ import React from 'react'
 const sections = ["All", "THEME", "PRICE", "DURATION", "CUSTOMER REVIEWS","MY TRIPS"];
 function SubNavofViewall() {
   return (
-    <nav className="bg-white sticky top-0 z-10 shadow-inner mt-20 mb-9">
-    <div className="max-w-7xl mx-auto px-4 py-4">
-      <p className="pb-2 italic underline font-medium em:hidden block">
-        Sub Menus
-      </p>
-      <ul className="flex flex-col em:flex-row items-start  em:items-center justify-center text-[.7rem] em:text-sm uppercase leading-6 font-normal  em:space-x-8">
+    <nav className="bg-white sticky mt-[38rem] sm:mt-[37rem] md:mt-[21rem] lg:mt-40 z-10 shadow-inner  mb-9">
+    <div className="max-w-7xl mx-auto px-0 em:px-4 py-4">
+ 
+      <ul className="flex  em:flex-row items-start  em:items-center justify-center text-[.7rem] em:text-sm uppercase leading-6 font-normal  em:space-x-8">
         {sections.map((item, idx) => (
           <button
             key={idx}
             className={`text-gray-500 em:py-0 py-2  em:border-b-0 ${
-              idx === sections.length - 1 ? "border-b-0" : "border-b"
+              idx === sections.length - 1 ? "border-b-0" : "border-b-0"
             }  em:w-auto  w-full hover:text-med-green`}
           >
             {item}

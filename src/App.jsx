bb889458a551efa7b14970b4a7118f5567bdcd7b
karan <jangdekaran54@gmail.com>
2024-11-17 23:19:59 +0530

    vercelerror

--- src/App.jsx
+++ src/App.jsx
@@ -22,7 +22,7 @@ import DestinationPage from "./components/Destinations/DestinationPage";
 import TravelTips from "./components/Travel-Tips/TravelTips";
 import AdminPanel from "./components/AdminPanel/AdminPanel";
 import StateDestination from "./components/Statedestination/StateDestination";
-import CountryDestination from "./components/CountryDestination/CountryDestination.jsx";
+import CountryDestination from "./components/CountryDestination/CountryDestination";
 
 function App() {
   const location = useLocation(); // Get the current route

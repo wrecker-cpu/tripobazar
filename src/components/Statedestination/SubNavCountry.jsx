111c4de31338a903d772de5f1bc1dca5b86a3bab
karan <jangdekaran54@gmail.com>
2024-11-25 14:14:12 +0530

    minorupdate

--- src/components/Statedestination/SubNavCountry.jsx
+++ src/components/Statedestination/SubNavCountry.jsx
@@ -7,6 +7,9 @@ function SubNavCountry({ toggleModal }) {
     <nav className="bg-white mt-[45rem] sm:mt-[40rem] md:mt-[26rem] lg:mt-60 sticky top-0 z-40 shadow-inner">
       <div className="max-w-7xl mx-auto px-4 py-4">
         <ul className="flex flex-row em:flex-row items-start scrollbar-hide px-4 overflow-x-scroll em:items-center justify-start em:justify-center text-[.7rem] em:text-sm uppercase leading-6 font-normal  gap-8">
+          <div className="cursor-pointer" onClick={toggleModal}>
+            <SmallerFilterSvg />
+          </div>
           {sections.map((item, idx) => (
             <button
               key={idx}
@@ -15,9 +18,6 @@ function SubNavCountry({ toggleModal }) {
               {item}
             </button>
           ))}
-          <div className="cursor-pointer" onClick={toggleModal}>
-            <SmallerFilterSvg />
-          </div>
         </ul>
       </div>
     </nav>

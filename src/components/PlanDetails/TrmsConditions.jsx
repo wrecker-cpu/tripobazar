64648c6cc74c64925e2bf57c6141eae6ba1ceb3f
DesaiPriyansh19 <priyanshdesai62@gmail.com>
2024-12-10 14:37:20 +0530

    polices added in plan details

---
+++ src/components/PlanDetails/TrmsConditions.jsx
@@ -0,0 +1,34 @@
+import React from 'react'
+
+function TrmsConditions() {
+  return (
+    <div className='w-full h-auto px-7 py-4'>
+     <p className='text-[#03B58B] font-semibold'>Travel Policy</p>
+    <p className='font-semibold'>1. Acceptance of Terms</p> <br></br>
+    <p className='mb-6 font-normal'>By using our website or mobile application, you acknowledge that you have read, 
+        understood, and agree to be bound by these terms and conditions.
+         If you do not agree with any part of these terms, please do not use our services.</p>
+         
+
+         <p className='font-semibold'>2. Booking Policy</p> <br></br>
+         <p className='mb-6 font-normal'>
+          <span className='font-semibold'> a.</span>  Booking Confirmation: All bookings are subject to availability and confirmation. Upon successful payment, you will receive a confirmation email with details of your reservation. Please review the details carefully and contact us immediately if any discrepancies are found.
+<br></br> <br></br>
+<span className='font-semibold'> b.</span>  Payment: We accept various payment methods, including credit cards and other secure payment gateways. Payments are processed securely, and your financial information is handled with the utmost confidentiality.
+<br></br> <br></br>
+<span className='font-semibold'> c.</span> Cancellation and Refund: Cancellation policies vary by service provider.
+ Please review the specific cancellation policy associated with your booking. Refund eligibility 
+is determined based on the terms and conditions of the service provider.</p>
+<br></br>
+
+<p className='font-semibold'>3. User Responsibilities</p> <br></br>
+<p className='mb-6 font-normal'>
+          <span className='font-semibold'> a.</span>  Accurate Information: Users are responsible for providing accurate and complete information during the booking process. Any inaccuracies may result in the cancellation of your reservation.
+<br></br> <br></br>
+
+<span className='font-semibold'> b.</span> Account Security: Users are responsible for maintaining the confidentiality of their account information, including passwords. Any unauthorized use of your account should be reported immediately.</p>
+    </div>
+  )
+}
+
+export default TrmsConditions
